cb214e1edbf1f9bc9b19d158c3f7b09920300048
hhoing <juwin501@naver.com>
2026-06-02 08:46:47 +0900

    add firmware

---
+++ include/imgArray.h
@@ -0,0 +1,23 @@
+#ifndef IMGARRAY_H
+#define IMGARRAY_H
+#include <stdint.h>
+
+extern const int imsArr[];
+
+extern const uint32_t ssuLogo[]; // 220 x 220
+
+extern const uint32_t humidLogo[]; // 60 x 60
+extern const uint32_t tempLogo[]; 
+
+extern const uint32_t baroTemperature[]; // 60 x 
+extern const uint32_t baroHumidity[]; // 60 x 60
+extern const uint32_t baroAtmosphere[]; // 60 x 90
+extern const uint32_t ssuLogo2[]; // 100 x 100
+extern const uint32_t ssuLogo3[]; // 65 x 65
+extern const uint32_t mold[];
+extern const uint32_t tomato[];
+extern const uint8_t happy[];
+extern const uint8_t sad[];
+#endif
+
+
